11d052e95f2d9d9db783881778e3326033c1cdf7
sumarokovlyosha-ops <sumarokov.lyosha@gmail.com>
2026-07-23 08:23:37 +0300

    feat: add application settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,18 @@
+from functools import lru_cache
+
 from pydantic_settings import BaseSettings,SettingsConfigDict
 
 class Settings(BaseSettings):
     name: str = "Education Platform API"
     version: str = "0.1.0"
-    debug: bool = "False"
+    debug: bool = False
     log_level: str = "INFO"
     model_config = SettingsConfigDict(
         env_prefix="APP_",
         env_file=".env",
         env_file_encoding="utf-8"
     )
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
-from app.api.routers.health import router as health_router
 
-app = FastAPI()
+from app.api.routers.health import router as health_router
+from app.core.config import get_settings
+
+settings = get_settings()
+
+app = FastAPI(
+    title=settings.name,
+    version=settings.version,
+    debug=settings.debug
+)
 
 app.include_router(health_router)
