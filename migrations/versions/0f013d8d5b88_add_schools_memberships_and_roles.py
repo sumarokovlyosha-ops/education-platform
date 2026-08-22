f6e39aea754e8b7a06022c620b232a8cad67ba41
sumarokovlyosha-ops <sumarokov.lyosha@gmail.com>
2026-08-22 22:36:45 +0300

    feat: add school membership and role models

--- migrations/versions/0f013d8d5b88_add_schools_memberships_and_roles.py
+++ migrations/versions/0f013d8d5b88_add_schools_memberships_and_roles.py
@@ -5,16 +5,15 @@ Revises: 7ea781a97a00
 Create Date: 2026-08-22 22:13:38.264543
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 revision: str = '0f013d8d5b88'
-down_revision: Union[str, Sequence[str], None] = '7ea781a97a00'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = '7ea781a97a00'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
